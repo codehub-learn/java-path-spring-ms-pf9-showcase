7997855151256b39b8690f83413909fb8deb7a05
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-11-22 21:10:26 +0200

    Removed method that should be relocated.

--- java-path-spring-cloud-catalog-service/src/main/java/gr/codelearn/spring/cloud/showcase/catalog/service/ProductServiceImpl.java
+++ java-path-spring-cloud-catalog-service/src/main/java/gr/codelearn/spring/cloud/showcase/catalog/service/ProductServiceImpl.java
@@ -3,12 +3,9 @@ package gr.codelearn.spring.cloud.showcase.catalog.service;
 import gr.codelearn.spring.cloud.showcase.catalog.domain.Product;
 import gr.codelearn.spring.cloud.showcase.catalog.repository.ProductRepository;
 import gr.codelearn.spring.cloud.showcase.core.service.BaseServiceImpl;
-import gr.codelearn.spring.cloud.showcase.core.transfer.KeyValue;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -24,9 +21,9 @@ public class ProductServiceImpl extends BaseServiceImpl<Product> implements Prod
 	public Product findBySerial(String serial) {
 		return productRepository.findBySerial(serial);
 	}
-
-	@Override
-	public List<KeyValue<String, Integer>> findProductSaleFrequency() {
-		return productRepository.findProductSaleFrequency();
-	}
+//
+//	@Override
+//	public List<KeyValue<String, Integer>> findProductSaleFrequency() {
+//		return productRepository.findProductSaleFrequency();
+//	}
 }
