861ef94ccc68189af5af3c7d854b772b2c5e9bff
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-11-23 00:22:24 +0200

    We need to scan two packages for candidate Spring beans; The core module and the current one.

--- java-path-spring-cloud-catalog-service/src/main/java/gr/codelearn/spring/cloud/showcase/catalog/CatalogApplication.java
+++ java-path-spring-cloud-catalog-service/src/main/java/gr/codelearn/spring/cloud/showcase/catalog/CatalogApplication.java
@@ -2,8 +2,10 @@ package gr.codelearn.spring.cloud.showcase.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"gr.codelearn.spring.cloud.showcase.core", "gr.codelearn.spring.cloud.showcase.catalog"})
 public class CatalogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogApplication.class, args);
