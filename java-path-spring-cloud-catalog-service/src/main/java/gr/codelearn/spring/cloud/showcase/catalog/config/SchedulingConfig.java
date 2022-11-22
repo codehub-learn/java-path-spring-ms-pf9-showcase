4552af8e1908813c581cd74ba3a364a80590a773
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-11-23 00:23:43 +0200

    Enabled scheduling task support.

---
+++ java-path-spring-cloud-catalog-service/src/main/java/gr/codelearn/spring/cloud/showcase/catalog/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package gr.codelearn.spring.cloud.showcase.catalog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
