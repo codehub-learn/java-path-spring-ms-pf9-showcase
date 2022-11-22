47405d9281acef1cc9a2bd8b4d3f2833ce307de0
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-11-23 00:23:22 +0200

    Enabled caching support and enabled cache for specific service methods.

--- java-path-spring-cloud-catalog-service/src/main/java/gr/codelearn/spring/cloud/showcase/catalog/service/CategoryServiceImpl.java
+++ java-path-spring-cloud-catalog-service/src/main/java/gr/codelearn/spring/cloud/showcase/catalog/service/CategoryServiceImpl.java
@@ -4,16 +4,36 @@ import gr.codelearn.spring.cloud.showcase.catalog.domain.Category;
 import gr.codelearn.spring.cloud.showcase.catalog.repository.CategoryRepository;
 import gr.codelearn.spring.cloud.showcase.core.service.BaseServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
 @Service
 @RequiredArgsConstructor
+@CacheConfig(cacheNames = "catalog", keyGenerator = "customCacheKeyGenerator")
 public class CategoryServiceImpl extends BaseServiceImpl<Category> implements CategoryService {
 	private final CategoryRepository categoryRepository;
 
 	@Override
 	public JpaRepository<Category, Long> getRepository() {
 		return categoryRepository;
+	}
+
+	@Override
+	@Cacheable
+	public Category findById(Long id) {
+		logger.trace("Retrieving entity with id {}.", id);
+		return getRepository().findById(id).orElseThrow(NoSuchElementException::new);
+	}
+
+	@Override
+	@Cacheable
+	public List<Category> findAll() {
+		logger.trace("Retrieving all entities.");
+		return categoryRepository.findAll();
 	}
 }
--- java-path-spring-cloud-catalog-service/src/main/java/gr/codelearn/spring/cloud/showcase/catalog/service/ProductServiceImpl.java
+++ java-path-spring-cloud-catalog-service/src/main/java/gr/codelearn/spring/cloud/showcase/catalog/service/ProductServiceImpl.java
@@ -4,11 +4,17 @@ import gr.codelearn.spring.cloud.showcase.catalog.domain.Product;
 import gr.codelearn.spring.cloud.showcase.catalog.repository.ProductRepository;
 import gr.codelearn.spring.cloud.showcase.core.service.BaseServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
 @Service
 @RequiredArgsConstructor
+@CacheConfig(cacheNames = "catalog", keyGenerator = "customCacheKeyGenerator")
 public class ProductServiceImpl extends BaseServiceImpl<Product> implements ProductService {
 	private final ProductRepository productRepository;
 
@@ -18,10 +24,25 @@ public class ProductServiceImpl extends BaseServiceImpl<Product> implements Prod
 	}
 
 	@Override
+	@Cacheable
+	public Product findById(Long id) {
+		logger.trace("Retrieving entity with id {}.", id);
+		return productRepository.findById(id).orElseThrow(NoSuchElementException::new);
+	}
+
+	@Override
+	@Cacheable
+	public List<Product> findAll() {
+		logger.trace("Retrieving all entities.");
+		return productRepository.findAll();
+	}
+
+	@Override
+	@Cacheable
 	public Product findBySerial(String serial) {
 		return productRepository.findBySerial(serial);
 	}
-//
+
 //	@Override
 //	public List<KeyValue<String, Integer>> findProductSaleFrequency() {
 //		return productRepository.findProductSaleFrequency();
