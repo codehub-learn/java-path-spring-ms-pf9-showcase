aa2190fd37516a3f6b7aae345475727ca9982409
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-11-23 11:00:47 +0200

    Trimmed empty line(s).

--- java-path-spring-cloud-catalog-service/src/main/java/gr/codelearn/spring/cloud/showcase/catalog/service/ProductService.java
+++ java-path-spring-cloud-catalog-service/src/main/java/gr/codelearn/spring/cloud/showcase/catalog/service/ProductService.java
@@ -5,6 +5,4 @@ import gr.codelearn.spring.cloud.showcase.core.service.BaseService;
 
 public interface ProductService extends BaseService<Product, Long> {
 	Product findBySerial(String serial);
-
-//	List<KeyValue<String, Integer>> findProductSaleFrequency();
 }
