a73a8ed03d13688e46353b0825d288ee5d48fdc4
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-11-23 13:05:36 +0200

    Added the port range to bind and homogenized the profile name used in bootstrap class to match other modules.

--- java-path-spring-cloud-mail-service/src/main/java/gr/codelearn/spring/cloud/showcase/mail/bootstrap/MailSampleGenerator.java
+++ java-path-spring-cloud-mail-service/src/main/java/gr/codelearn/spring/cloud/showcase/mail/bootstrap/MailSampleGenerator.java
@@ -7,10 +7,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-@Profile("sample-mail")
+@Profile("generate-mail")
 @Component
 @RequiredArgsConstructor
-public class SampleMailRunner extends BaseComponent implements CommandLineRunner {
+public class MailSampleGenerator extends BaseComponent implements CommandLineRunner {
 	private final MailService mailService;
 
 	@Override
