95fcbbc410fda5a5daa23bac669ab5241d14cc7a
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-12-07 23:02:40 +0200

    Added order service as Eureka Discovery server client, added a customer service Feign client along with a bootstrap class to showcase client-side load balancing.

--- java-path-spring-cloud-order-service/src/main/java/gr/codelearn/spring/cloud/showcase/order/OrderApplication.java
+++ java-path-spring-cloud-order-service/src/main/java/gr/codelearn/spring/cloud/showcase/order/OrderApplication.java
@@ -2,10 +2,14 @@ package gr.codelearn.spring.cloud.showcase.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(namedQueriesLocation = "classpath:jpa-named-queries.properties")
+@EnableDiscoveryClient
+@EnableFeignClients
 public class OrderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApplication.class, args);
--- java-path-spring-cloud-order-service/src/main/java/gr/codelearn/spring/cloud/showcase/order/service/client/CatalogServiceClient.java
+++ java-path-spring-cloud-order-service/src/main/java/gr/codelearn/spring/cloud/showcase/order/service/client/CatalogServiceClient.java
@@ -2,11 +2,16 @@ package gr.codelearn.spring.cloud.showcase.order.service.client;
 
 import gr.codelearn.spring.cloud.showcase.core.transfer.ApiResponse;
 import gr.codelearn.spring.cloud.showcase.core.transfer.resource.ProductResource;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@FeignClient(name = "catalog-service", path = "/products")
 public interface CatalogServiceClient {
 	@GetMapping(params = {"serial"})
 	ResponseEntity<ApiResponse<ProductResource>> findBySerial(@RequestParam(name = "serial") String serial);
+
+	@GetMapping("lb")
+	ResponseEntity<ApiResponse<String>> checkLoadBalancing();
 }
