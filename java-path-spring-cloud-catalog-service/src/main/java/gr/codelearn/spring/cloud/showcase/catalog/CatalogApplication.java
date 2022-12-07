0bad23fd9bd914f5924b05f01bf5acc2c20cee9d
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-12-07 22:49:49 +0200

    Added customer service as Eureka Discovery server client, so it can be registered and discovered by other services.

--- java-path-spring-cloud-catalog-service/src/main/java/gr/codelearn/spring/cloud/showcase/catalog/CatalogApplication.java
+++ java-path-spring-cloud-catalog-service/src/main/java/gr/codelearn/spring/cloud/showcase/catalog/CatalogApplication.java
@@ -2,10 +2,12 @@ package gr.codelearn.spring.cloud.showcase.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"gr.codelearn.spring.cloud.showcase.core", "gr.codelearn.spring.cloud.showcase.catalog"})
+@EnableDiscoveryClient
 public class CatalogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogApplication.class, args);
